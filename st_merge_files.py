7b28391b50ebd05a6e933392fc2b4392c762fd8c
OlgaShodorova <osnsnq@gmail.com>
2025-09-22 13:07:49 +0300

    Update st_merge_files.py

--- st_merge_files.py
+++ st_merge_files.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_pagePconfig(page_title='Объединение данных экспериментов🔬', layout='wide')
+st.set_page_config(page_title='Объединение данных экспериментов🔬', layout='wide')
 
 hour = st.numder_input('Введите время')
 uploaded_data_file = st.file_uploader('Загрузите соответсвующие файлы', type='xlsx', accept_multiple_files=True)
